1315d631daa9809c0f694669c47b9ff4ec18094c
Alexander Fehr <afehr35@outlook.com>
2023-02-24 20:02:56 +0100

    Implemented rest

--- SoundRipper/index.js
+++ SoundRipper/index.js
@@ -1,1 +1,27 @@
-import './style.css'
+import "./style.css";
+
+//Get form from web
+const form = document.querySelector("form");
+
+//On submit form
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  //TODO Check if the link is valid
+
+  //Add the data from the form into the FormData datastructure
+  const formData = new FormData(form);
+
+  //Send post request and await reponse
+  const response = await fetch("http://localhost:8080/download", {
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json",
+    },
+    body: JSON.stringify({
+      link: formData.get("link_input"),
+      quality: formData.get("quality_input"),
+    }),
+  });
+
+});
--- SoundRipper/server.js
+++ SoundRipper/server.js
@@ -1,17 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+
+import cors from "cors";
+
 import ffmpeg from "fluent-ffmpeg"; 
-import ytdl from "ytdl";
+import ytdl from "ytdl-core";
 
 
 //Init express
 const app =  express();
 
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //On post request
 app.post('/download', async (req, res) => {
-
-
-
+    console.log(req.body);
+    res.send("200");
 });
 
 //Run express on port 8080
